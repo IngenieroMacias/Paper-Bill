4b6feb1b13afddc5daaf6b6ff177b614f04f6619
Edward Alejandro Macias Gil <ingenieromaciasgil@gmail.com>
2019-06-18 16:14:27 -0500

    Replanteo de Dependencias

--- src/app/componentes/nav-bar/nav-bar.component.ts
+++ src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-bar',
+  selector: 'app-navbar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
