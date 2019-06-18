dbe63fc00e313fde1b30e2c22100d2e5f648664e
Edward Alejandro Macias Gil <ingenieromaciasgil@gmail.com>
2019-06-18 16:44:30 -0500

    Creacion de Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './componentes/home-page/home-page.component';
+import {LoginPageComponent} from './componentes/login-page/login-page.component';
+import {RegisterPageComponent} from './componentes/register-page/register-page.component';
+import {PrivatePageComponent} from './componentes/private-page/private-page.component';
+import {NotFoundPageComponent} from './componentes/not-found-page/not-found-page.component'
+import { from } from 'rxjs';
+const routes: Routes = [
+  {path:'',component: HomePageComponent},
+  {path:'login',component: LoginPageComponent},
+  {path:'register',component: RegisterPageComponent},
+  {path:'private',component: PrivatePageComponent},
+  {path:'**',component: NotFoundPageComponent},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
