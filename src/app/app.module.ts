759b139b2aee5a31c0dc9a73abdac4d7b32df228
Edward Alejandro Macias Gil <ingenieromaciasgil@gmail.com>
2019-06-18 15:16:46 -0500

    Creacion de Componentes Principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { RegisterPageComponent } from './componentes/register-page/register-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { PrivatePageComponent } from './componentes/private-page/private-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    PrivatePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
