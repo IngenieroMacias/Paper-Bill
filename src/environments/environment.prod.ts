2579ebf5204eb73a08a92235df6ad7983c5b8588
Edward Alejandro Macias Gil <ingenieromaciasgil@gmail.com>
2019-06-19 21:11:23 -0500

    Configuracion Enviroments Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBEknBTG1PIPi7B8K5qQezIJPSLr1b9ygI",
+    authDomain: "loginwebstore.firebaseapp.com",
+    databaseURL: "https://loginwebstore.firebaseio.com",
+    projectId: "loginwebstore",
+    storageBucket: "loginwebstore.appspot.com",
+    messagingSenderId: "192898367392",
+    appId: "1:192898367392:web:ec1bf67ee9d4fbba"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+  apiKey: "AIzaSyBEknBTG1PIPi7B8K5qQezIJPSLr1b9ygI",
+  authDomain: "loginwebstore.firebaseapp.com",
+  databaseURL: "https://loginwebstore.firebaseio.com",
+  projectId: "loginwebstore",
+  storageBucket: "loginwebstore.appspot.com",
+  messagingSenderId: "192898367392",
+  appId: "1:192898367392:web:ec1bf67ee9d4fbba"
+  }
 };
 
 /*
